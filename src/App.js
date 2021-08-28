6e83feb5b98814443c37432828acf14838833854
Timothy Karani <kartimothy@gmail.com>
2021-08-28 12:30:49 +0300

    Tested out axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+
+export const sendLogin = (data) => {
+  axios.defaults.withCredentials = true;
+  const response = axios.get('http://localhost:8100/sanctum/csrf-cookie',
+    // {
+    //   withCredentials: false
+    // }
+  ).then(response => {
+    return axios.post('http://localhost:8100/api/auth/login', data, {
+      xsrfHeaderName: "X-XSRF-TOKEN", // change the name of the header to "X-XSRF-TOKEN" and it should works
+      withCredentials: true
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  })
+  return response;
+}
+
 function App() {
+  console.log("Jean");
+
+  const params = new URLSearchParams();
+  params.append('email', 'johndoe@gmail.com');
+  params.append('password', 'rastaman');
+
+  sendLogin(params);
+
   return (
     <div className="App">
       <header className="App-header">
