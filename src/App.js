75d49fe6a1b423f0f479593a11f8e885ac3fe2e6
Timothy Karani <kartimothy@gmail.com>
2021-08-30 01:23:11 +0300

    base UI working

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
-import { Button, Grid } from '@material-ui/core';
+import SignInScreen from './pages/auth/SignIn';
 
-const axios = require('axios');
-
-// Make a request for a user with a given ID
-
-export const sendLogin = (data) => {
-  axios.defaults.withCredentials = true;
-  const response = axios.get('https://laravel-erp-server.herokuapp.com/sanctum/csrf-cookie',
-    // {
-    //   withCredentials: false
-    // }
-  ).then(response => {
-    return axios.post('https://laravel-erp-server.herokuapp.com/api/auth/login', data, {
-      xsrfHeaderName: "X-XSRF-TOKEN", // change the name of the header to "X-XSRF-TOKEN" and it should works
-      withCredentials: true
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  })
-  return response;
-}
 
 function App() {
-  console.log("Jean");
-
-  const params = new URLSearchParams();
-  params.append('email', 'johndoe@gmail.com');
-  params.append('password', 'rastaman');
-
-  sendLogin(params);
-
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: '100vh' }}
-    >
-
-      <Grid item xs={3}>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </Grid>
-
-    </Grid>
+    <SignInScreen></SignInScreen>
   );
 }
 
---
+++ src/pages/auth/SignIn.js
@@ -0,0 +1,180 @@
+import {
+  Button, Grid, Card, CardContent, TextField, makeStyles,
+  Typography,
+  createTheme,
+  ThemeProvider
+} from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+import axios from 'axios';
+import { useState } from 'react';
+
+// Make a request for a user with a given ID
+export const sendLogin = (data) => {
+  axios.defaults.withCredentials = true;
+  const response = axios.get('https://laravel-erp-server.herokuapp.com/sanctum/csrf-cookie'
+  ).then(response => {
+    return axios.post('https://laravel-erp-server.herokuapp.com/api/auth/login', data, {
+      xsrfHeaderName: "X-XSRF-TOKEN", // change the name of the header to "X-XSRF-TOKEN" and it should works
+      withCredentials: true
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  })
+  return response;
+}
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Open Sans',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  textField: {
+    minWidth: "100%"
+  },
+  header: {
+    textAlign: "center",
+    fontSize: "2.7rem",
+    fontWeight: 200
+  },
+  headerHelper: {
+    textAlign: "center",
+    color: grey[700],
+    fontSize: "0.9rem"
+  },
+  signInButton: {
+    minWidth: "100%",
+    padding: "0.5rem"
+  },
+  forgotButton: {
+    minWidth: "100%",
+  }
+}));
+
+function SignInScreen() {
+  // const params = new URLSearchParams();
+  // params.append('email', 'johndoe@gmail.com');
+  // params.append('password', 'rastaman');
+
+  // sendLogin(params);
+
+  const classes = useStyles();
+
+  const [values, setValues] = useState({
+    email: "johndoe@gmail.com",
+    password: ""
+  });
+
+  const emailChanged = event => {
+    setValues(event.target.value);
+  };
+
+  // const fieldChangedClosure = fieldName => {
+  //   return function fieldChanged(event) {
+
+  //   }
+  // }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+
+        <Grid item xs={3}>
+          <Card>
+            <CardContent>
+
+              <Grid container spacing={2}>
+
+                <Grid container spacing={1}>
+                  <Grid item xs={12}>
+                    <Typography variant="h2"
+                      className={classes.header}
+                    >
+                      Sign In
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography
+                      variant="body1"
+                      className={classes.headerHelper}
+                      gutterBottom
+                    >
+                      Enter your credentials to proceed
+                    </Typography>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    label="Email Address"
+                    variant="outlined"
+                    value={values["email"]}
+                    onChange={emailChanged}
+                    className={classes.textField}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    className={classes.textField}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.signInButton}
+                  >
+                    Sign In
+                  </Button>
+
+                  <Button
+                    color="primary"
+                    className={classes.forgotButton}
+                  >
+                    Forgot Password
+                  </Button>
+                </Grid>
+
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default SignInScreen;
