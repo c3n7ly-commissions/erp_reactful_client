a9b5e377be4748791da9b1073caa0b3f9b52d0c0
Timothy Karani <kartimothy@gmail.com>
2021-09-04 09:51:35 +0300

    Refactored to use base url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://laravel-erp-server.herokuapp.com';
 
 ReactDOM.render(
   // For snackbars to work
--- src/utils/auth/auth.js
+++ src/utils/auth/auth.js
@@ -8,25 +8,19 @@ class Auth {
   // Make a request for a user with a given ID
   login = (data, successCallback, errorCallback) => {
     axios.defaults.withCredentials = true;
-    const response = axios
-      .get('https://laravel-erp-server.herokuapp.com/sanctum/csrf-cookie')
-      .then((_response) => {
-        axios
-          .post(
-            'https://laravel-erp-server.herokuapp.com/api/auth/login',
-            data,
-            {
-              xsrfHeaderName: 'X-XSRF-TOKEN', // change the name of the header to "X-XSRF-TOKEN"
-              withCredentials: true,
-            }
-          )
-          .then(function (response) {
-            successCallback(response);
-          })
-          .catch(function (error) {
-            errorCallback(error);
-          });
-      });
+    const response = axios.get('/sanctum/csrf-cookie').then((_response) => {
+      axios
+        .post('/api/auth/login', data, {
+          xsrfHeaderName: 'X-XSRF-TOKEN', // change the name of the header to "X-XSRF-TOKEN"
+          withCredentials: true,
+        })
+        .then(function (response) {
+          successCallback(response);
+        })
+        .catch(function (error) {
+          errorCallback(error);
+        });
+    });
     return response;
   };
 }
