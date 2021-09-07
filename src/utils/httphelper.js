83c588a2008789bec6dfc0b63d9daf618e9632ac
Timothy Karani <kartimothy@gmail.com>
2021-09-07 11:46:50 +0300

    added common error handler

--- src/utils/httphelper.js
+++ src/utils/httphelper.js
@@ -88,6 +88,54 @@ class HttpHelper {
         });
     });
   }
+
+  handleCommonErrors(error, snackbarStateHandler) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      if (error.response.status === 401) {
+        // unauthenticated
+        snackbarStateHandler({
+          open: true,
+          type: 'error',
+          value: error.response.data.message,
+          redirect: '/',
+          duration: 1500,
+        });
+      } else if (error.response.status === 409) {
+        // conflict
+        snackbarStateHandler({
+          open: true,
+          type: 'warning',
+          value: error.response.data.message,
+        });
+      } else if (error.response.status === 422) {
+        // unprocessable entity
+        snackbarStateHandler({
+          open: true,
+          type: 'warning',
+          value: error.response.data.message,
+        });
+      } else {
+        snackbarStateHandler({
+          open: true,
+          type: 'error',
+          value: `error code ${error.response.status}`,
+        });
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+  }
 }
 
 export default new HttpHelper();
